50867ca181e8637f3f9412ab15cfefa2adecdb06
TheStrix <parthbhatia98@gmail.com>
2015-03-30 11:35:33 -0400

    [PATCH] Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.
    
    Original author: Mike Kasick <mike@kasick.org>
    
    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to hammerhead, it is based of my mako/grouper port, which is
      based of Asus TF201 patch ported by Jens Andersen <jens.andersen@gmail.com>
    
      Hammerhead is the first device I've ported this patch to which uses
      device tree binary instead of atags. It is also rather picky - its
      bootloader erases first 128MB of RAM on reboot, so following hacks had
      to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.
    
      Same as my mako/flo patches, the guest kernel does not require any
      modifications, kexec-hardboot is equivalent to standard kexec.
    
    Parth Bhatia <parthbhatia98@gmail.com>:
      Ported from https://gist.github.com/Tasssadar/7833796
      Adapted for armani
    
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -164,6 +164,17 @@ static void __init msm8226_early_memory(void)
 
 static void __init msm8226_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page, just before the ram_console
+	//struct membank* bank = &meminfo.bank[0];
+	//phys_addr_t start = bank->start + bank->size - SZ_1M - 0x00300000;
+	phys_addr_t start = KEXEC_HB_PAGE_ADDR;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	reserve_info = &msm8226_reserve_info;
 	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8226_reserve_table);
 	msm_reserve();
